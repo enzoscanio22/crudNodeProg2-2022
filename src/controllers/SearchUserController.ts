00afa28693c449d1738f135819b75501d51b72e6
tpCodeX <ponce.code.mza@gmail.com>
2022-09-02 23:11:26 -0300

    traduccion terminada

--- src/controllers/SearchUserController.ts
+++ src/controllers/SearchUserController.ts
@@ -16,7 +16,7 @@ class SearchUserController {
       });
     } catch (err) {
       response.render("message", {
-        message: `Erro ao buscar usuário: ${err.message}`
+        message: `Error al buscar usuario: ${err.message}`
       });
     }
   }
