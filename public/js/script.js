56f296ee85a56a2897a54c82e80d8e80020f54e3
enzoscanio22 <enzoscanio555@gmail.com>
2022-10-18 15:11:20 -0300

    Edicion del Backgraund a dark

--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,16 @@ function counter() {
   let countdown = 2;
 
   let timerDiv = document.getElementById("timer");
+  // let path = window.location.href;
+  // path=path.split("/")
+  // let path2=path[0]+"//"+path[2]+"/"+path[3]
 
   let timer = setInterval(function () {
     timerDiv.innerHTML;
     countdown--;
     if (countdown === 0) {
       clearInterval(timer);
-      window.location.href = "../"
+      window.location.href = "../" //path2
     }
   }, 1000);
 }
